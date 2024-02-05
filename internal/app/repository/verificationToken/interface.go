18d4fabb13de361a1efd9b6b6ac7ee4629f20447
Sergey Dyakov <s.dyakov9@gmail.com>
2024-02-05 04:06:00 +0300

    refactor: new dataservice->repository project structure

--- internal/app/repository/verificationToken/interface.go
+++ internal/app/repository/verificationToken/interface.go
@@ -2,7 +2,7 @@ package verificationtoken
 
 import m "auth-service/internal/app/model"
 
-type VerificationTokenInterface interface {
+type Repository interface {
 	Create(newVerificationToken *m.VerificationToken, email string) error
 	Get(condition map[string]interface{}) (*m.VerificationToken, error)
 	Delete(condition map[string]interface{}) error
