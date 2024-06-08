7dc364f210b7e7b5109ea9bd95ed15247676ec3e
Sergey Dyakov <s.dyakov9@gmail.com>
2024-06-09 01:22:11 +0300

    add xid default

--- migrations/20240319102043_auth_user_tokens.sql
+++ migrations/20240319102043_auth_user_tokens.sql
@@ -3,7 +3,7 @@
 SELECT 'up SQL query';
 -- +goose StatementEnd
 CREATE TABLE IF NOT EXISTS public.user_tokens (
-  user_id public.xid NOT NULL,
+  user_id public.xid NOT NULL DEFAULT xid(),
   number BIGINT NOT NULL,
   purpose INTEGER NOT NULL,
   secret CHAR(64) NOT NULL,
